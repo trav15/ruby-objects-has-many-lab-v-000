efc6b23d5c04afae71b217e2380a8c9803613cff
Travis T <travistokuyama@gmail.com>
2018-09-23 18:08:44 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,5 +1,18 @@
-class Posts 
-  attr_accessor :post, :author
-  
-  
+class Post
+  attr_accessor :title, :author
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    author.name if author
+  end
 end
