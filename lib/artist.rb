36c81181b4ff81ab18c25e065dd0c8d02b6dc3fb
Travis T <travistokuyama@gmail.com>
2018-09-23 18:02:44 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,12 +11,11 @@ class Artist
   end
   
   def add_song(song)
-    @songs << song
     song.artist = self
   end
-  
-  def add_song_by_name(song)
-    @songs << song
+
+  def add_song_by_name(name)
+    song = Song.new(name)
     song.artist = self
   end
   
