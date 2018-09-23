cd8be91a63203a935ae377ad7100834e65871cd0
Travis T <travistokuyama@gmail.com>
2018-09-23 18:08:17 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,17 +1,31 @@
-class Author 
-  attr_accessor :name 
-  
+class Author
+  attr_accessor :name
+
+  @@all = []
+
   def initialize(name)
     @name = name
-    @posts = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
   end
 
   def posts
-    @posts
+    Post.all.select {|post| post.author == self}
   end
-  
-  def add_post
-    
+
+  def self.post_count
+    Post.all.count
   end
-  
 end
